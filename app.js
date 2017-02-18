d2eb3e8420539f984bc8e16a55b03c0c34f9cd9d
William Teder <wteder@hydreon.com>
2017-02-17 20:18:24 -0600

    OAuth works, Asana works :D

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const Asana = require("asana"),
+"use strict";
+
+const Asana = require('asana'),
 	  cookieParser = require('cookie-parser'),
-	  Config = require("./src/config");
+	  session = require('express-session'),
+	  Config = require('./src/config');
 
 global.config = new Config("config/config.json");
 
@@ -37,19 +40,30 @@ function createClient() {
 
 // Causes request cookies to be parsed, populating `req.cookies`.
 app.use(cookieParser());
+app.use(session({
+	secret: global.config.get("app.session_secret"), resave: false, saveUninitialized: true, cookie: { maxAge: 60000 }
+}));
 
 // Home page - shows user name if authenticated, otherwise seeks authorization.
 app.get("/", (req, res) => {
+	if(req.query.redirect_uri)
+		req.session.alexa = req.query;
+
 	const client = createClient(),
 		  token = req.cookies.token;
 
+	req.session.token = token;
+
 	if(token) {
+		if(token.indexOf("___") === -1)
+			return false;
+
 		client.useOauth({
-			credentials: token
+			credentials: token.split("___")[0]
 		});
 
-		client.users.me().then((me) => {
-			res.end("Hello " + me.name);
+		client.users.me().then(() => {
+			res.redirect(req.session.alexa.redirect_uri + "#access_token=" + token + "&state=" + req.session.alexa.state + "&token_type=bearer&code=");
 		}).catch((err) => {
 			res.end("Error fetching user: " + err);
 		});
@@ -57,18 +71,25 @@ app.get("/", (req, res) => {
 		res.redirect(client.app.asanaAuthorizeUrl());
 });
 
+app.post("/authorize_alexa", (req, res) => {
+	const alexa = req.session.alexa;
+
+	res.redirect(alexa.redirect_uri + "#access_token=" + req.session.token + "state=" + alexa.state + "&token_type=bearer");
+});
+
 // Authorization callback - redirected to from Asana.
 app.get("/oauth_callback", (req, res) => {
-	const code = req.params.code;
+	const code = req.query.code;
 
 	if(code) {
 		const client = createClient();
 		client.app.accessTokenFromCode(code).then((credentials) => {
-			res.cookie("token", credentials.access_token, { maxAge: 60 * 60 * 1000 });
-			// Todo: Get refresh token, save both on server
+			console.log(credentials);
 
-			res.redirect('/');
+			res.cookie("token", credentials.access_token + "___" + credentials.refresh_token, { maxAge: 60 * 60 * 1000 });
+
+			res.redirect("/");
 		});
 	} else
-		res.end("Error getting authorization: " + req.params.error);
+		res.end("Error getting authorization: " + req.query.error);
 });
--- apps/asana/app.js
+++ apps/asana/app.js
@@ -6,11 +6,9 @@
  */
 
 // Dependencies
-const alexa = require("alexa-app"),
-	  asana = require("asana"),
-	  log = require("../../src/log");
-
-const client = asana.Client.create().useAccessToken('my_access_token');
+const alexa = require('alexa-app'),
+	  Asana = require('asana'),
+	  log = require('../../src/log');
 
 // Allow this module to be reloaded automatically when code is changed
 module.change_code = 1;
@@ -18,40 +16,108 @@ module.change_code = 1;
 // Define an alexa-app
 const app = new alexa.app("asana");
 
+const asanaClients = {},
+	  state = {}; // TODO: Save state information to database
+
+const STATE_READY = 0,
+	  STATE_SELECTING_WORKSPACE = 1;
+
+app.pre = (req, res) => {
+	let accessToken = req.sessionDetails.accessToken;
+
+	if(accessToken && accessToken.indexOf("___") === -1)
+		accessToken = false;
+	
+	if(!accessToken) {
+		res.linkAccount().shouldEndSession(true).say("Hello! Please link your Asana account in the Alexa app.").send();
+
+		return false;
+	}
+
+	const tmp = accessToken.split("___"),
+		  asanaAccessToken = tmp[0],
+		  refreshToken = tmp[1];
+
+	const client = Asana.Client.create();
+
+	client.useOauth({
+		credentials: {
+			token_type: "bearer",
+			access_token: asanaAccessToken,
+			refresh_token: refreshToken,
+			expires_in: 3600,
+			data: {}
+		},
+	});
+
+	if(!asanaClients[req.sessionId]) {
+		asanaClients[req.sessionId] = client;
+		state[req.sessionId] = {
+			workspaceId: -1
+		};
+	}
+
+	return true;
+};
+
 // Called when user says "open asana"
-app.launch((req, res) => {
-	res.say("Hello! I should say something about the state of your account.");
-	res.shouldEndSession(false, "Anything else?");
+app.launch((req, res, send) => {
+	asanaClients[req.sessionId].users.me()
+		.then((user) => {
+			const userFirstName = user.name.split(" ")[0] || false;
+
+			let response = "Hello" + (", " + userFirstName || "") + "! ";
+
+			if(user.workspaces.length > 1) {
+				response += "I'm going to list off the workspaces on your Asana account. When I'm done, say the number of the workspace you want to use.";
+				state[req.sessionId] = STATE_SELECTING_WORKSPACE;
+
+				let workspaceCounter = 0;
+				for(let workspace of user.workspaces) {
+					workspaceCounter++;
+
+					response += " Say \"" + workspaceCounter + "\" to work in " + workspace.name + ".";
+				}
+
+				response += " Okay, which numbered workspace should I use?";
+			} else {
+				state[req.sessionId] = STATE_READY;
+				state[req.sessionId].workspaceId = user.workspaces[0];
+
+				response += "You can ask me about upcoming tasks, to mark tasks as complete, or to create a new task.";
+			}
+
+			res.say(response).shouldEndSession(false);
+			send();
+		});
+
+	return false;
 });
 
 app.intent("GetUpcomingTasks", {
 		"slots": {
-			"name": "Timeframe",
-			"type": "AMAZON.DATE"
+			"Timeframe": "AMAZON.DATE"
 		},
 		"utterances": [
-			"what is on my schedule {Timeframe}",
-			"what is due {Timeframe}",
-			"what do I have to do {Timeframe}",
+			"what is on my schedule {-|Timeframe}",
+			"what's on my schedule {-|Timeframe}",
+			"what is due {-|Timeframe}",
+			"what's due {-|Timeframe}",
+			"what do I {have|need} to do {-|Timeframe}",
 			"what are my upcoming tasks"
 		]
 	}, (req, res) => {
-		res.linkAccount();
-		res.say("Check the Alexa app to log into Asana.");
+
 	}
 );
 
-app.pre = (request, response, type) => {
-	if(request.sessionDetails.application.applicationId !== global.config.get("")) {
-		// Fail ungracefully
-		throw 'Invalid applicationId: ' + request.sessionDetails.application.applicationId;
-	}
-};
-
 // Last-resort error method (unknown intent, etc)
 app.post = (request, response, type, exception) => {
-	if(exception)
-		response.say("Sorry, an error occurred.").send();
+	if(exception) {
+		response.say("Sorry, an error occurred!").send();
+		console.log(exception);
+		console.log(exception.stack);
+	}
 };
 
 log.info("System", "Alexa app successfully initialized.");
