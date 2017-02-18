b04b03abdb939e8bc99bceb03a26c3956015c648
William Teder <wteder@hydreon.com>
2017-02-17 20:59:46 -0600

    Fix refresh token for Asana, finish workspace selection

--- apps/asana/app.js
+++ apps/asana/app.js
@@ -38,7 +38,11 @@ app.pre = (req, res) => {
 		  asanaAccessToken = tmp[0],
 		  refreshToken = tmp[1];
 
-	const client = Asana.Client.create();
+	const client = Asana.Client.create({
+		clientId: global.config.get("asana.client_id"),
+		clientSecret: global.config.get("asana.client_secret"),
+		redirectUri: global.config.get("asana.redirect_uri")
+	});
 
 	client.useOauth({
 		credentials: {
@@ -64,27 +68,30 @@ app.pre = (req, res) => {
 app.launch((req, res, send) => {
 	asanaClients[req.sessionId].users.me()
 		.then((user) => {
+			console.log(user);
+
 			const userFirstName = user.name.split(" ")[0] || false;
 
 			let response = "Hello" + (", " + userFirstName || "") + "! ";
 
 			if(user.workspaces.length > 1) {
-				response += "I'm going to list off the workspaces on your Asana account. When I'm done, say the number of the workspace you want to use.";
-				state[req.sessionId] = STATE_SELECTING_WORKSPACE;
+				response += "Welcome to Asana for Alexa. I'm going to list off and number the workspaces on your Asana account. Listen for the one you want to work in.";
+				state[req.sessionId].userState = STATE_SELECTING_WORKSPACE;
+				state[req.sessionId].workspaces = user.workspaces;
 
 				let workspaceCounter = 0;
 				for(let workspace of user.workspaces) {
 					workspaceCounter++;
 
-					response += " Say \"" + workspaceCounter + "\" to work in " + workspace.name + ".";
+					response += " \"" + workspaceCounter + "\" is " + workspace.name + ".";
 				}
 
-				response += " Okay, which numbered workspace should I use?";
+				response += " Okay, which workspace number should I use?";
 			} else {
-				state[req.sessionId] = STATE_READY;
-				state[req.sessionId].workspaceId = user.workspaces[0];
+				state[req.sessionId].userState = STATE_READY;
+				state[req.sessionId].workspace = user.workspaces[0];
 
-				response += "You can ask me about upcoming tasks, to mark tasks as complete, or to create a new task.";
+				response += "You can ask me about upcoming tasks, to mark tasks as complete, or to create a new task. What do you want to do?";
 			}
 
 			res.say(response).shouldEndSession(false);
@@ -107,7 +114,39 @@ app.intent("GetUpcomingTasks", {
 			"what are my upcoming tasks"
 		]
 	}, (req, res) => {
+		console.log(req.data.request.intent);
+	}
+);
 
+app.intent("SelectWorkspace", {
+		"slots": {
+			"WorkspaceId": "AMAZON.NUMBER"
+		},
+		"utterances": [
+			"{-|WorkspaceId}"
+		]
+	}, (req, res) => {
+		if(state[req.sessionId].userState !== STATE_SELECTING_WORKSPACE) {
+			res.say("Sorry, but you've already selected a workspace.");
+
+			return true;
+		}
+
+		try {
+			const userWorkspaceNumber = parseInt(req.data.request.intent.slots.WorkspaceId.value) - 1,
+				workspace = state[req.sessionId].workspaces[userWorkspaceNumber];
+
+			if(workspace) {
+				state[req.sessionId].userState = STATE_READY;
+				state[req.sessionId].workspace = workspace;
+
+				res.say("Great! You can ask me about upcoming tasks, to mark tasks as complete, or to create a new task. What do you want to do?").shouldEndSession(false);
+				delete state[req.sessionId].workspaces;
+			} else
+				res.say("Sorry, I don't see that workspace in your account. Try again.").shouldEndSession(false);
+		} catch(e) {
+			res.say("Sorry, I didn't get that. Try again.").shouldEndSession(false);
+		}
 	}
 );
 
